816a13e411c3196d14ab7dd5b05d0f78f4d72052
Lv MingChuan <mingchuan.lv@shopee.com>
2020-09-08 23:00:56 +0800

    i also live!

--- src/main/java/DarkLaunch.java
+++ src/main/java/DarkLaunch.java
@@ -1,8 +1,11 @@
+import com.google.common.io.CharStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -36,10 +39,18 @@ public class DarkLaunch {
       DarkRuleConfig ruleConfig = null;
       try {
         in = this.getClass().getResourceAsStream("/dark-rule.yaml");
+        System.out.println("debug===========");
+        System.out.println(in.toString());
+          String text = CharStreams.toString(new InputStreamReader(in, "UTF-8"));
+          System.out.println(text);
         if (in != null) {
           Yaml yaml = new Yaml(); // why bug ?
           ruleConfig = yaml.loadAs(in, DarkRuleConfig.class);
         }
+      } catch (UnsupportedEncodingException e) {
+          e.printStackTrace();
+      } catch (IOException e) {
+          e.printStackTrace();
       } finally {
         if (in != null) {
           try {
